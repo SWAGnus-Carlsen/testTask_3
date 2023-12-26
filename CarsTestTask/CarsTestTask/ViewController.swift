5149215b2a1467a2fbfc88162eba35c22fcae0d1
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2023-12-26 16:28:11 +0300

    Project was created

---
+++ CarsTestTask/CarsTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarsTestTask
+//
+//  Created by Vitaliy Halai on 26.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
